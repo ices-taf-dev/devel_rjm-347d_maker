050b1cecf40e54ed1bb3ff8557bfa643dd4f9a76
Arni Magnusson <arnima@hafro.is>
2017-12-15 16:00:03 +0100

    Rename xtra_plot.R -> report.R

--- all.R
+++ all.R
@@ -1,17 +1,17 @@
-## Run core scripts: data, input, model, output
+## Run core scripts: data, input, model, output, report
 
 library(icesTAF)
 
-## Script     Output
-## ------     ------
-## data       survey.csv
-## input      input.RData
-## model      dls.txt
-## output     dls.txt
-## xtra_plot  dls.png
+## Script  Output
+## ------  ------
+## data    survey.csv
+## input   input.RData
+## model   dls.txt
+## output  dls.txt
+## report  dls.png
 
-make("data.R",      "data.R",            "data/survey.csv")
-make("input.R",     "data/survey.csv",   "input/input.RData")
-make("model.R",     "input/input.RData", "model/dls.txt")
-make("output.R",    "model/dls.txt",     "output/dls.txt")
-make("xtra_plot.R", "output/dls.txt",    "report/plot/dls.png")
+make("data.R",   "data.R",            "data/survey.csv")
+make("input.R",  "data/survey.csv",   "input/input.RData")
+make("model.R",  "input/input.RData", "model/dls.txt")
+make("output.R", "model/dls.txt",     "output/dls.txt")
+make("report.R", "output/dls.txt",    "report/dls.png")
