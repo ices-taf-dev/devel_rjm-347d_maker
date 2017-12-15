cb28559733c38a00922c63c283e08ee16c0c2d89
Arni Magnusson <arnima@hafro.is>
2017-12-15 15:34:31 +0100

    Rename db -> data

--- all.R
+++ all.R
@@ -1,18 +1,18 @@
-## Run core scripts: db, input, model, output, upload
+## Run core scripts: data, input, model, output, upload
 
 library(icesTAF)
 
 ## Script     Output
 ## ------     ------
-## db         survey.csv
+## data       survey.csv
 ## input      input.RData
 ## model      dls.txt
 ## output     dls.txt
 ## upload     dls.txt
 ## xtra_plot  dls.png
 
-make("db.R",        "db.R",                   "db/survey.csv")
-make("input.R",     "db/survey.csv",          "input/input.RData")
+make("data.R",      "data.R",                 "data/survey.csv")
+make("input.R",     "data/survey.csv",        "input/input.RData")
 make("model.R",     "input/input.RData",      "model/dls.txt")
 make("output.R",    "model/dls.txt",          "output/dls.txt")
 make("upload.R",    "output/dls.txt",         "upload/output/dls.txt")
--- data.R
+++ data.R
@@ -1,17 +1,17 @@
 ## Download and preprocess data, write TAF input tables
 
 ## Before: catch.csv, surveys_all.csv (ftp)
-## After:  catch.csv, survey.csv, summary.csv (db)
+## After:  catch.csv, survey.csv, summary.csv (data)
 
 library(icesTAF)
 
-ftp <- "https://raw.githubusercontent.com/ices-taf/ftp/master/wgef/2015/rjm-347d/"
+ftp <- "https://raw.githubusercontent.com/ices-taf/ftp/master/wgef/2015/rjm-347d/raw/"
 
-mkdir("db")
+mkdir("data")
 
 ## Download data, select years and surveys of interest
-catch <- read.taf(paste0(ftp, "db/catch.csv"))
-survey <- read.taf(paste0(ftp, "db/surveys_all.csv"))
+catch <- read.taf(paste0(ftp, "catch.csv"))
+survey <- read.taf(paste0(ftp, "surveys_all.csv"))
 survey <- survey[survey$Year %in% 1993:2014, names(survey) != "Unknown"]
 
 ## Scale each survey to average 1, combine index as average of three surveys
@@ -23,7 +23,7 @@ row.names(survey) <- NULL
 summary <- data.frame(Year=survey$Year, Catch=NA, Index=survey$Index)
 summary$Catch[summary$Year %in% catch$Year] <- catch$Catch
 
-## Write tables to db directory
-write.taf(catch, "db/catch.csv")
-write.taf(survey, "db/survey.csv")
-write.taf(summary, "db/summary.csv")
+## Write tables to data directory
+write.taf(catch, "data/catch.csv")
+write.taf(survey, "data/survey.csv")
+write.taf(summary, "data/summary.csv")
